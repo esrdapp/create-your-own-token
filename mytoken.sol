827c3c52120ea51f51094392971b045aa94fb1f1
ESR <82104698+esrdapp@users.noreply.github.com>
2021-09-12 23:40:05 +0100

    Update mytoken.sol

--- mytoken.sol
+++ mytoken.sol
@@ -1,49 +1,40 @@
 /* ---------------------------------------------
-Create your own HPB token (HRC20) which you can deploy onto the HPB blockchain, and then add to HPDex. Simply change the information on lines 114 to 118. 
-*/ ---------------------------------------------
+Create your own HPB token (HRC20) which you can deploy onto the HPB blockchain, and then add to HPDex. Simply change the information on lines 98 to 102. 
+ ---------------------------------------------
+*/
 
 pragma solidity ^0.4.4;
-
 contract Token {
-
     /// @return total amount of tokens
     function totalSupply() constant returns (uint256 supply) {}
-
     /// @param _owner The address from which the balance will be retrieved
     /// @return The balance
     function balanceOf(address _owner) constant returns (uint256 balance) {}
-
     /// @notice send `_value` token to `_to` from `msg.sender`
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
     function transfer(address _to, uint256 _value) returns (bool success) {}
-
     /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
     /// @param _from The address of the sender
     /// @param _to The address of the recipient
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
-
     /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @param _value The amount of wei to be approved for transfer
     /// @return Whether the approval was successful or not
     function approve(address _spender, uint256 _value) returns (bool success) {}
-
     /// @param _owner The address of the account owning tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @return Amount of remaining tokens allowed to spent
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
-
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
     
 }
-
 contract MyToken is Token {
-
     function transfer(address _to, uint256 _value) returns (bool success) {
         //Default assumes totalSupply can't be over max (2^256 - 1).
         //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
@@ -56,7 +47,6 @@ contract MyToken is Token {
             return true;
         } else { return false; }
     }
-
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
         //same as above. Replace this line with the following if you want to protect against wrapping uints.
         //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
@@ -68,33 +58,26 @@ contract MyToken is Token {
             return true;
         } else { return false; }
     }
-
     function balanceOf(address _owner) constant returns (uint256 balance) {
         return balances[_owner];
     }
-
     function approve(address _spender, uint256 _value) returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
     }
-
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
       return allowed[_owner][_spender];
     }
-
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
     uint256 public totalSupply;
 }
-
 contract ERC20Token is MyToken {
-
     function () {
         //if ether is sent to this address, send it back.
         throw;
     }
-
     /* Public variables of the token */
 
     /*
@@ -103,6 +86,7 @@ contract ERC20Token is MyToken {
     They allow one to customise the token contract & in no way influences the core functionality.
     Some wallets/interfaces might not even bother to look at this information.
     */
+    
     string public name;                  //fancy name
     uint8 public decimals;               //How many decimals to show
     string public symbol;                //An identifier
